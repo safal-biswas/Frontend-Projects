f5337b231915fb6771ad4d3aac9bc0816d829952
safal-biswas <safalbiswas005@gmail.com>
2024-09-25 13:33:02 +0600

    Upload Project 3

--- Project 3/script.js
+++ Project 3/script.js
@@ -1,3 +1,13 @@
-img{
-    width: 100%
-}
+const open = document.getElementById('open');
+const close = document.getElementById('close');
+const container = document.querySelector('.container');
+
+open.addEventListener('click', () => {
+    container.classList.add('show-nav');
+    console.log('open click');
+});
+
+close.addEventListener('click', () => {
+    container.classList.remove('show-nav');
+    console.log('close click');
+});
